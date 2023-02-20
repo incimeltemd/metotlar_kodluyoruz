d6a8d6265df17b508ac68ebf15ad967979db8c51
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-20 23:43:25 +0300

    ödev yapıldı

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+
+        int sonuc = topla(a,b);
+        Console.WriteLine(sonuc);
+
+        metotlar ornek = new metotlar();
+        ornek.yazdir(Convert.ToString(sonuc));
+
+    }
+    static int topla(int deger1, int deger2)
+    {
+        return(deger1+deger2);
+    }
+}
+class metotlar
+{
+    public void yazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+}
